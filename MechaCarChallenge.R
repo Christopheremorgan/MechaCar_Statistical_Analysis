683f43003b83cc2228f44f6338153e35c3301aaf
Chris Morgan <courtneychris@me.com>
2021-07-24 17:38:02 -0400

    summary statistics on coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,12 @@
 library(dplyr)
+library(tidyverse)
 
+# Multivate regression to predict MPG
 mecha_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_df) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_df)) #generate summary statistics
+
+# Summary statistics on suspension coils
+coil_tbl <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- coil_tbl %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create total summary table
+lot_summary <- coil_tbl %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create lot summary table
